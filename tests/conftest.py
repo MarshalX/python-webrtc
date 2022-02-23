0e5cd7dce3eff51ac63273f476225154b738e343
Il'ya (Marshal) <ilya@marshal.by>
2022-02-24 00:25:04 +0100

    add tests for addTrack method of RTCPeerConnection interface

---
+++ tests/conftest.py
@@ -0,0 +1,51 @@
+#
+#  Copyright 2022 Il`ya (Marshal) <https://github.com/MarshalX>. All rights reserved.
+#
+#  Use of this source code is governed by a BSD-style license
+#  that can be found in the LICENSE.md file in the root of the project.
+#
+
+import pytest
+
+import webrtc
+
+
+# alias for rtc_peer_connection
+@pytest.fixture
+def pc(rtc_peer_connection):
+    return rtc_peer_connection
+
+
+@pytest.fixture
+def rtc_peer_connection(request):
+    pc = webrtc.RTCPeerConnection()
+
+    def close_pc():
+        pc.close()
+
+    request.addfinalizer(close_pc)
+
+    return pc
+
+
+def get_stream(constraints, request):
+    # TODO pass constraints when cpp part will be ready
+    stream = webrtc.get_user_media()
+
+    def stop_tracks():
+        for track in stream.get_tracks():
+            track.stop()
+
+    request.addfinalizer(stop_tracks)
+
+    return stream
+
+
+@pytest.fixture
+def audio_stream(request):
+    return get_stream(None, request)
+
+
+@pytest.fixture
+def video_stream(request):
+    return get_stream(None, request)
